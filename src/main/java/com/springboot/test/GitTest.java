4b098b8db757807ca8344f5643328328e653f29e
chenliangtest <cheng3417885@163.com>
2017-12-16 16:31:10 +0800

    sit

--- src/main/java/com/springboot/test/GitTest.java
+++ src/main/java/com/springboot/test/GitTest.java
@@ -7,6 +7,10 @@ public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("git 命令测试合并冲突");
+        gitError();
+    }
+
+    public static void gitError(){
         System.out.println("git 命令测试合并冲突");
     }
 }
