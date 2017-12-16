2432234eeab5f94c26fcab7e69b6ac52adf68ac5
chenliangtest <cheng3417885@163.com>
2017-12-16 16:22:19 +0800

    git 测试合并分支冲突

---
+++ src/main/java/com/springboot/test/GitTest.java
@@ -0,0 +1,11 @@
+package com.springboot.test;
+
+/**
+ * Created by Administrator on 2017/12/16.
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
