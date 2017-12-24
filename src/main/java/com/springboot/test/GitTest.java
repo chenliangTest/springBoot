50ca1f4bbb532ea3c0525a1f11d5a1464617b2c0
chenliangtest <cheng3417885@163.com>
2017-12-24 13:37:42 +0800

    333

--- src/main/java/com/springboot/test/GitTest.java
+++ src/main/java/com/springboot/test/GitTest.java
@@ -12,7 +12,7 @@ public class GitTest {
 
     public static void gitError(){
         System.out.println("git 命令测试合并冲突111111");
-
         System.out.println("git 命令测试合并冲突22223333333");
+        System.out.println("git 命令测试合并冲突444");
     }
 }
