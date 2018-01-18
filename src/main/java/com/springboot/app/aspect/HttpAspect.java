047356b25b18bec38fcbdb1ba6783264dba5fd96
chenliang <w_chenliang@hfbank.com.cn>
2018-01-18 08:29:49 +0800

    aop

--- src/main/java/com/springboot/app/aspect/HttpAspect.java
+++ src/main/java/com/springboot/app/aspect/HttpAspect.java
@@ -1,15 +1,37 @@
 package com.springboot.app.aspect;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+/**
+ * @ClassName: HttpAspect
+ * @Description: aop给所有方法执行前添加判断，是否登录
+ * @author: chenliang
+ * @date: 2018/1/17 下午3:43
+ */
 @Aspect
 @Component
 public class HttpAspect {
 
-    @Before("execution(public * com.springboot.app.contorll.*(..))")
+    private static Logger logger  = LoggerFactory.getLogger(HttpAspect.class);
+
+    @Pointcut("execution(public * com.springboot.app.contorll.*.*(..))")
     public void log(){
-        System.out.println("请登录！！");
+
+    }
+
+    @Before("log()")
+    public void doBefore(){
+        logger.info("1111111");
+    }
+
+    @After("log()")
+    public void doAfter(){
+        logger.info("2222222");
     }
 }
--- src/main/java/com/springboot/app/contorll/UserControll.java
+++ src/main/java/com/springboot/app/contorll/UserControll.java
@@ -3,8 +3,14 @@ package com.springboot.app.contorll;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @ClassName: UserContorll
+ * @Description: 用户信息
+ * @author: chenliang
+ * @date: 2018/1/17 下午3:39
+ */
 @RestController
-public class UserContorll {
+public class UserControll {
 
     @GetMapping(value = "/getUser")
     public void queryUserList(){
